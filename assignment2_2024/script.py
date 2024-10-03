4d896b7ff215c84e5d656646644bc00638ce8e97
Anna Doan <annabhdoan@gmail.com>
2024-10-03 18:30:49 +0200

    feat: insert activity and trackpoints

--- assignment2_2024/script.py
+++ assignment2_2024/script.py
@@ -1,10 +1,15 @@
 from DbConnector import DbConnector
+import os
 
 class GeolifeProgram:
     def __init__(self):
         self.connection = DbConnector()
         self.db_connection = self.connection.db_connection
         self.cursor = self.connection.cursor
+        self.users = os.listdir("./dataset/dataset/Data")
+        self.users = sorted([user for user in self.users if user.isdigit()], key=lambda x: int(x))
+        self.labels = open("./dataset/dataset/labeled_ids.txt", "r").read().split("\n")
+
 
     def create_tables(self):
         user_table = """CREATE TABLE IF NOT EXISTS User (
@@ -37,6 +42,27 @@ class GeolifeProgram:
         self.db_connection.commit()
 
         print("Tables user, activity and trackpoint created")
+        print(self.users)
+        print("Lable table: ", self.labels)
+
+    def insert_user_data(self, table_name):
+        for user_id in self.users:
+            if user_id in self.labels:
+                query = "INSERT INTO %s (id, has_labels) VALUES ('%s', True)"
+                self.cursor.execute(query % (table_name, user_id))
+            else:
+                query = "INSERT INTO %s (id, has_labels) VALUES ('%s', False)"
+                self.cursor.execute(query % (table_name, user_id))
+        self.db_connection.commit()
+
+    def insert_activity_data(self, table_name):
+        query = "INSERT INTO %s (user_id, transportation_mode, start_date_time, end_date_time) VALUES ('%s', '%s', '%s', '%s')"
+        # self.cursor.execute(query % (table_name, user_id, transportation_mode, start_date_time, end_date_time))
+
+
+    def insert_trackpoint_data(self, table_name):
+        query = "INSERT INTO %s (activity_id, lat, lon, altitude, date_days, date_time) VALUES (%d, %f, %f, %d, %f, '%s')"
+        # self.cursor.execute(query % (table_name, activity_id, lat, lon, altitude, date_days, date_time))
 
     def show_tables(self):
         self.cursor.execute("SHOW TABLES")
@@ -51,6 +77,9 @@ def main():
     try:
         program = GeolifeProgram()
         program.create_tables()
+        program.insert_user_data(table_name="User")
+        # program.insert_activity_data(table_name="Activity")
+        # program.insert_trackpoint_data(table_name="Trackpoint")
         program.show_tables()
     except Exception as e:
         print("ERROR: Failed to create tables:", e)
